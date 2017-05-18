54ded799047e0ceed91eb57c2584a0b9dbce02ba
iwan roberts <ilroberts@gmail.com>
2017-05-18 20:56:26 +0100

    initial commit

---
+++ ToDoApp/Models/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoApp.Models
+{
+    public class TodoItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Key { get; set; }
+        public string Name { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
